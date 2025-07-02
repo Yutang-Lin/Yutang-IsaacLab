2457747d91ce9dccc320ea9e7c600172383bbc4c
Yutang-Lin <yutang.lin@stu.pku.edu.cn>
2025-07-02 19:47:57 +0800

    add distillation runner

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/runners/__init__.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/runners/__init__.py
@@ -1,4 +1,5 @@
 from rsl_rl.runners.on_policy_runner import OnPolicyRunner
 from .base_runner import BaseRunner
+from .distill_runner import DistillRunner
 
-__all__ = ["OnPolicyRunner", "BaseRunner"]
+__all__ = ["OnPolicyRunner", "BaseRunner", "DistillRunner"]
---
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/runners/distill_runner.py
@@ -0,0 +1,331 @@
+
+from __future__ import annotations
+
+import os
+import statistics
+import time
+import torch
+
+import rsl_rl
+from rsl_rl.env import VecEnv
+from collections import deque
+from rsl_rl.utils import store_code_state
+
+from isaaclab_rl.rsl_rl.algorithms import *
+from isaaclab_rl.rsl_rl.modules import *
+
+from .base_runner import BaseRunner
+
+class DistillRunner(BaseRunner):
+    """Distillation runner for training and evaluation."""
+
+    def __init__(self, env: VecEnv, train_cfg: dict, log_dir: str | None = None, device="cpu"):
+        self.cfg = train_cfg
+        self.alg_cfg = train_cfg["algorithm"]
+        self.policy_cfg = train_cfg["policy"]
+        self.device = device
+        self.env = env
+        self.env_unwrapped = self.env.unwrapped # type: ignore
+
+        # check if multi-gpu is enabled
+        self._configure_multi_gpu()
+
+        # resolve training type depending on the algorithm
+        if self.alg_cfg["class_name"] == "PPO":
+            self.training_type = "rl"
+        elif self.alg_cfg["class_name"] == "Distillation":
+            self.training_type = "distillation"
+        else:
+            print(f"Warning: Training type not found for algorithm {self.alg_cfg['class_name']}, using rl as default.")
+            self.training_type = "rl"
+
+        # resolve dimensions of observations
+        obs, extras = self.env.get_observations()
+        num_obs = obs.shape[1]
+
+        assert hasattr(self.env_unwrapped, "distill_step"), "Environment must have a distill_step method"
+        _, targets = self.env_unwrapped.distill_step()
+        num_targets = targets.shape[1]
+
+        # resolve type of privileged observations
+        if self.training_type == "rl":
+            if "critic" in extras["observations"]:
+                self.privileged_obs_type = "critic"  # actor-critic reinforcement learnig, e.g., PPO
+            else:
+                self.privileged_obs_type = None
+        if self.training_type == "distillation":
+            if "teacher" in extras["observations"]:
+                self.privileged_obs_type = "teacher"  # policy distillation
+            else:
+                self.privileged_obs_type = None
+
+        # resolve dimensions of privileged observations
+        if self.privileged_obs_type is not None:
+            num_privileged_obs = extras["observations"][self.privileged_obs_type].shape[1]
+        else:
+            num_privileged_obs = num_obs
+
+        # evaluate the policy class
+        policy_class = eval(self.policy_cfg.pop("class_name"))
+        self.policy: ActorCritic | ActorCriticRecurrent | StudentTeacher | StudentTeacherRecurrent = policy_class(
+            num_obs, num_privileged_obs, self.env.num_actions, **self.policy_cfg
+        ).to(self.device)
+
+        self.learning_rate = self.alg_cfg['learning_rate']
+        self.num_learning_epochs = self.alg_cfg["num_learning_epochs"]
+        self.policy_optimizer = torch.optim.AdamW(self.policy.parameters(), lr=self.learning_rate)
+
+        # store training configuration
+        self.num_steps_per_env = self.cfg["num_steps_per_env"]
+        self.save_interval = self.cfg["save_interval"]
+        self.empirical_normalization = self.cfg["empirical_normalization"]
+        if self.empirical_normalization:
+            self.obs_normalizer = EmpiricalNormalization(shape=[num_obs], until=1.0e8).to(self.device)
+            self.privileged_obs_normalizer = EmpiricalNormalization(shape=[num_privileged_obs], until=1.0e8).to(
+                self.device
+            )
+        else:
+            self.obs_normalizer = torch.nn.Identity().to(self.device)  # no normalization
+            self.privileged_obs_normalizer = torch.nn.Identity().to(self.device)  # no normalization
+
+        self.obs_buffer = torch.zeros(self.env.num_envs, self.num_steps_per_env, num_obs, device=self.device, requires_grad=False)
+        self.target_buffer = torch.zeros(self.env.num_envs, self.num_steps_per_env, num_targets, device=self.device, requires_grad=False)
+
+        # Decide whether to disable logging
+        # We only log from the process with rank 0 (main process)
+        self.disable_logs = self.is_distributed and self.gpu_global_rank != 0
+        # Logging
+        self.log_dir = log_dir
+        self.writer = None
+        self.tot_timesteps = 0
+        self.tot_time = 0
+        self.current_learning_iteration = 0
+        self.git_status_repos = [rsl_rl.__file__]
+
+    def learn(self, num_learning_iterations: int, init_at_random_ep_len: bool = False):  # noqa: C901
+        # initialize writer
+        if self.log_dir is not None and self.writer is None and not self.disable_logs:
+            # Launch either Tensorboard or Neptune & Tensorboard summary writer(s), default: Tensorboard.
+            self.logger_type = self.cfg.get("logger", "tensorboard")
+            self.logger_type = self.logger_type.lower()
+
+            if self.logger_type == "neptune":
+                from rsl_rl.utils.neptune_utils import NeptuneSummaryWriter
+
+                self.writer = NeptuneSummaryWriter(log_dir=self.log_dir, flush_secs=10, cfg=self.cfg)
+                self.writer.log_config(self.env.cfg, self.cfg, self.alg_cfg, self.policy_cfg)
+            elif self.logger_type == "wandb":
+                from rsl_rl.utils.wandb_utils import WandbSummaryWriter
+
+                self.writer = WandbSummaryWriter(log_dir=self.log_dir, flush_secs=10, cfg=self.cfg)
+                self.writer.log_config(self.env.cfg, self.cfg, self.alg_cfg, self.policy_cfg)
+            elif self.logger_type == "tensorboard":
+                from torch.utils.tensorboard import SummaryWriter # type: ignore
+
+                self.writer = SummaryWriter(log_dir=self.log_dir, flush_secs=10)
+            else:
+                raise ValueError("Logger type not found. Please choose 'neptune', 'wandb' or 'tensorboard'.")
+
+        # start learning
+        obs, extras = self.env.get_observations()
+        privileged_obs = extras["observations"].get(self.privileged_obs_type, obs)
+        obs, privileged_obs = obs.to(self.device), privileged_obs.to(self.device)
+        self.train_mode()  # switch to train mode (for dropout for example)
+
+        # Book keeping
+        ep_infos = []
+
+        # Ensure all parameters are in-synced
+        if self.is_distributed:
+            print(f"Synchronizing parameters for rank {self.gpu_global_rank}...")
+            self.alg.broadcast_parameters()
+            # TODO: Do we need to synchronize empirical normalizers?
+            #   Right now: No, because they all should converge to the same values "asymptotically".
+
+        # Start training
+        start_iter = self.current_learning_iteration
+        tot_iter = start_iter + num_learning_iterations
+        for it in range(start_iter, tot_iter):
+            start = time.time()
+            # Rollout
+            with torch.inference_mode():
+                for n in range(self.num_steps_per_env):
+                    # Step the environment
+                    observations, targets = self.env_unwrapped.distill_step()
+                    # Move to device
+                    obs = observations['policy'].to(self.device)
+                    targets = targets.to(self.device)
+
+                    self.obs_buffer[:, n] = obs
+                    self.target_buffer[:, n] = targets
+
+                stop = time.time()
+                collection_time = stop - start
+                start = stop
+            
+            mean_loss = 0.0
+            for _ in range(self.num_learning_epochs):
+                # Sample actions
+                actions = self.policy.act_inference(self.obs_buffer.clone().flatten(0, 1))
+                loss = ((actions - self.target_buffer.clone().flatten(0, 1)) ** 2).mean(dim=0).sum()
+                mean_loss += loss.item()
+
+                self.policy_optimizer.zero_grad()
+                loss.backward()
+                self.policy_optimizer.step()
+
+            mean_loss /= self.num_learning_epochs
+            # update policy
+            loss_dict = {"mean_loss": mean_loss}
+
+            stop = time.time()
+            learn_time = stop - start
+            self.current_learning_iteration = it
+            # log info
+            if self.log_dir is not None and not self.disable_logs:
+                # Log information
+                self.log(locals())
+                # Save model
+                if it % self.save_interval == 0:
+                    self.save(os.path.join(self.log_dir, f"model_{it}.pt"))
+
+            # Clear episode infos
+            ep_infos.clear()
+            # Save code state
+            if it == start_iter and not self.disable_logs:
+                # obtain all the diff files
+                git_file_paths = store_code_state(self.log_dir, self.git_status_repos)
+                # if possible store them to wandb
+                if self.logger_type in ["wandb", "neptune"] and git_file_paths:
+                    for path in git_file_paths:
+                        self.writer.save_file(path) # type: ignore
+
+        # Save the final model after training
+        if self.log_dir is not None and not self.disable_logs:
+            self.save(os.path.join(self.log_dir, f"model_{self.current_learning_iteration}.pt"))
+
+    def log(self, locs: dict, width: int = 80, pad: int = 35):
+        # Compute the collection size
+        collection_size = self.num_steps_per_env * self.env.num_envs * self.gpu_world_size
+        # Update total time-steps and time
+        self.tot_timesteps += collection_size
+        self.tot_time += locs["collection_time"] + locs["learn_time"]
+        iteration_time = locs["collection_time"] + locs["learn_time"]
+
+        # -- Episode info
+        ep_string = ""
+
+        mean_std = 0.0
+        fps = int(collection_size / (locs["collection_time"] + locs["learn_time"]))
+
+        # -- Losses
+        for key, value in locs["loss_dict"].items():
+            self.writer.add_scalar(f"Loss/{key}", value, locs["it"])
+        self.writer.add_scalar("Loss/learning_rate", self.learning_rate, locs["it"])
+
+        # -- Policy
+        self.writer.add_scalar("Policy/mean_noise_std", mean_std, locs["it"])
+
+        # -- Performance
+        self.writer.add_scalar("Perf/total_fps", fps, locs["it"])
+        self.writer.add_scalar("Perf/collection time", locs["collection_time"], locs["it"])
+        self.writer.add_scalar("Perf/learning_time", locs["learn_time"], locs["it"])
+
+        str = f" \033[1m Learning iteration {locs['it']}/{locs['tot_iter']} \033[0m "
+
+        log_string = (
+            f"""{'#' * width}\n"""
+            f"""{str.center(width, ' ')}\n\n"""
+            f"""{'Computation:':>{pad}} {fps:.0f} steps/s (collection: {locs[
+                'collection_time']:.3f}s, learning {locs['learn_time']:.3f}s)\n"""
+            f"""{'Mean action noise std:':>{pad}} {mean_std:.2f}\n"""
+        )
+        for key, value in locs["loss_dict"].items():
+            log_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
+
+        log_string += ep_string
+        log_string += (
+            f"""{'-' * width}\n"""
+            f"""{'Total timesteps:':>{pad}} {self.tot_timesteps}\n"""
+            f"""{'Iteration time:':>{pad}} {iteration_time:.2f}s\n"""
+            f"""{'Time elapsed:':>{pad}} {time.strftime("%H:%M:%S", time.gmtime(self.tot_time))}\n"""
+            f"""{'ETA:':>{pad}} {time.strftime(
+                "%H:%M:%S",
+                time.gmtime(
+                    self.tot_time / (locs['it'] - locs['start_iter'] + 1)
+                    * (locs['start_iter'] + locs['num_learning_iterations'] - locs['it'])
+                )
+            )}\n"""
+        )
+        print(log_string)
+
+    def save(self, path: str, infos=None):
+        # -- Save model
+        saved_dict = {
+            "model_state_dict": self.policy.state_dict(),
+            "optimizer_state_dict": self.policy_optimizer.state_dict(),
+            "iter": 0, # No iteration for distillation
+            "infos": infos,
+        }
+        # -- Save observation normalizer if used
+        if self.empirical_normalization:
+            saved_dict["obs_norm_state_dict"] = self.obs_normalizer.state_dict()
+            saved_dict["privileged_obs_norm_state_dict"] = self.privileged_obs_normalizer.state_dict()
+
+        # save model
+        torch.save(saved_dict, path)
+
+        # upload model to external logging service
+        if self.logger_type in ["neptune", "wandb"] and not self.disable_logs:
+            self.writer.save_model(path, self.current_learning_iteration) # type: ignore
+
+    def load(self, path: str, load_optimizer: bool = True):
+        loaded_dict = torch.load(path, weights_only=False)
+        # -- Load model
+        resumed_training = self.policy.load_state_dict(loaded_dict["model_state_dict"])
+        # -- Load observation normalizer if used
+        if self.empirical_normalization:
+            if resumed_training:
+                # if a previous training is resumed, the actor/student normalizer is loaded for the actor/student
+                # and the critic/teacher normalizer is loaded for the critic/teacher
+                self.obs_normalizer.load_state_dict(loaded_dict["obs_norm_state_dict"])
+                self.privileged_obs_normalizer.load_state_dict(loaded_dict["privileged_obs_norm_state_dict"])
+            else:
+                # if the training is not resumed but a model is loaded, this run must be distillation training following
+                # an rl training. Thus the actor normalizer is loaded for the teacher model. The student's normalizer
+                # is not loaded, as the observation space could differ from the previous rl training.
+                self.privileged_obs_normalizer.load_state_dict(loaded_dict["obs_norm_state_dict"])
+        # -- load optimizer if used
+        if load_optimizer and resumed_training:
+            # -- algorithm optimizer
+            self.policy_optimizer.load_state_dict(loaded_dict["optimizer_state_dict"])
+        # -- load current learning iteration
+        if resumed_training:
+            self.current_learning_iteration = loaded_dict["iter"]
+        return loaded_dict["infos"]
+
+    def get_inference_policy(self, device=None):
+        self.eval_mode()  # switch to evaluation mode (dropout for example)
+        if device is not None:
+            self.policy.to(device)
+        policy = self.policy.act_inference
+        if self.cfg["empirical_normalization"]:
+            if device is not None:
+                self.obs_normalizer.to(device)
+            policy = lambda x: self.policy.act_inference(self.obs_normalizer(x))  # noqa: E731
+        return policy
+
+    def train_mode(self):
+        # -- PPO
+        self.policy.train()
+        if self.empirical_normalization:
+            self.obs_normalizer.train()
+            self.privileged_obs_normalizer.train()
+
+    def eval_mode(self):
+        # -- PPO
+        self.policy.eval()
+        # -- Normalization
+        if self.empirical_normalization:
+            self.obs_normalizer.eval()
+            self.privileged_obs_normalizer.eval()
