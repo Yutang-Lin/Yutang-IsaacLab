cf19363a59202de80deef708b7d4f4a32cbc60fc
Yutang-Lin <yutang.lin@stu.pku.edu.cn>
2025-08-08 14:34:26 +0800

    update amp

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/amp_cfg.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/amp_cfg.py
@@ -20,8 +20,8 @@ class RslRlAmpCfg:
     learning_rate: float = 1e-3
     """The learning rate for the AMP module. Default is 1e-3."""
 
-    num_learning_epochs: int = 4
-    """The number of learning epochs for the AMP module. Default is 4."""
+    num_learning_epochs: int = 1
+    """The number of learning epochs for the AMP module. Default is 1."""
 
     hidden_dims: list[int] = [256, 256, 256]
     """The hidden dimensions for the AMP discriminator network. Default is [256, 256, 256]."""
